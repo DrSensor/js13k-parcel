80b1f8175abf8e986997386cecdb47fc4fb52001
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-08-31 16:51:06 +0700

    Use nodePackages and nixpkgs for node_modules
    Check if node-packages.nix#sources are in nodePackages or nixpkgs
    by comparing `nonScoped packageName` and `version` from sources
    with `pname` and `version` from nodePackages or nixpkgs.
    
    * if source in nodePackages then use it,
    * else if source in nixkpkgs then use that,
    * otherwise don't do anything, just use the sources.

--- flake.nix
+++ flake.nix
@@ -5,21 +5,64 @@
       # dlint and dprint only available on this systems. (github.com releases archieve)
       givenSystems = [ "x86_64-linux" "x86_64-darwin" "x86_64-windows" ];
 
+      # collection of hash value for external/patched packages
+      integrity = import ./integrity.nix;
+
       # Helpers to instantiate nixpkgs for supported system types.
       give = with nixpkgs.lib; let forAllSystems = f: genAttrs givenSystems (system: f system); in
       f: forAllSystems (system:
         let
           pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
-          node = with pkgs; callPackage ./package-lock.nix { };
+          node.shell = with pkgs.lib; let
+            inherit (pkgs) nodePackages callPackage nodeEnv;
+            node = callPackage ./node-packages.nix { };
+            args = overrideExisting node.args {
+              dependencies = map
+                (source@{ name, ... }:
+                  if integrity ? ${name} then
+                    (overrideExisting source rec {
+                      version =
+                        if nodePackages ? ${name} then nodePackages.${name}.version
+                        else pkgs.${name}.version;
+                      src = pkgs.fetchurl (integrity.${name} // {
+                        url = "https://registry.npmjs.org/${name}/-/${name}-${version}.tgz";
+                      });
+                    })
+                  else source)
+                (filter
+                  ({ packageName, ... }: # TODO: match version
+                    let pkgName = last (splitString "/" packageName); in
+                    nodePackages ? ${pkgName} || pkgs ? ${pkgName})
+                  node.args.dependencies);
+            };
+            nativeBuildInputs = with pkgs; with nodePackages; [ node-gyp-build pkg-config ];
+            nodeDependencies = filterAttrs
+              (name: drv:
+                let dependencies = map ({ packageName, ... }: last (splitString "/" packageName)) args.dependencies;
+                in any ({ packageName, ... }: packageName == name) dependencies)
+              nodePackages // pkgs;
+            nodeDependenciesWithout = drvs: filterAttrs (name: val: any (drv: drv.name == name) drvs) nodeDependencies;
+          in
+          with pkgs; (nodeEnv.buildNodeShell args).override {
+            inherit nativeBuildInputs;
+            buildInputs = [ vips ] ++ nodeDependenciesWithout nativeBuildInputs;
+            ESBUILD_BINARY_PATH = "${esbuild}/bin/esbuild";
+            # npmFlags = "--sharp-local-prebuilds=${vips}/bin/vips";
+            # preRebuild = ''
+            #   npm config set sharp_local_prebuilds ${vips}/bin/vips
+            #   # export npm_config_sharp_local_prebuilds=${vips}/bin/vips
+            #   export ESBUILD_BINARY_PATH=${esbuild}/bin/esbuild
+            # '';
+          };
         in
         f { inherit pkgs node; inherit (pkgs) system lib stdenv; });
 
       # check if node2nix (in devShell) already executed
-      lockExists = builtins.pathExists ./package-lock.nix;
+      lockExists = builtins.pathExists ./node-packages.nix;
     in
     {
-      # nix develop
-      devShell = give ({ pkgs, lib, node, ... }: with pkgs; mkShell {
+      #> nix develop
+      devShell = give ({ pkgs, node, lib, ... }: with pkgs; mkShell {
         inputsFrom = lib.optionals lockExists [ node.shell ];
         packages = with nodePackages; [ node2nix ]
           ++ [ dlint dprint ];
@@ -27,12 +70,13 @@
         shellHook = with node.shell;
           if lockExists then ''
             [ -d "node_modules" ] || ln -s ${nodeDependencies}/lib/node_modules node_modules
-          '' else ''
-            echo "generating package-lock.nix node-pacakges.nix node-env.nix"
-            node2nix --composition package-lock.nix \
+          ''
+          else ''
+            echo "generating node-pacakges.nix node-env.nix"
+            node2nix --composition /dev/null \
                      --strip-optional-dependencies \
                      --development
-            [ -d ".git" ] && git add package-lock.nix node-packages.nix node-env.nix
+            [ -d ".git" ] && git add node-packages.nix node-env.nix
 
             echo -e "\nPlease run$(tput bold) nix develop $(tput sgr0)again."
             exit
@@ -40,31 +84,45 @@
       });
 
       # extra pkgs
-      overlay = final: prev: with prev.lib; mapAttrs
-        (name: value@{ sha256, bin ? name, ... }: with final; let
+      overlay = final: prev: with prev.lib; (mapAttrs
+        (pname: value@{ version ? "latest", bin ? pname, ... }: with final; let
           target = with systems.parse; tripleFromSystem (mkSystemFromString system);
+          integrity' = mapAttrs
+            (name: package:
+              if package ? ${system} then package.${system} else
+              if package ? ${target} then package.${target}
+              else package)
+            integrity;
         in
-        optionalAttrs (value ? github || value ? url)
-          (stdenv.mkDerivation {
-            inherit name;
-            # since it only available in .zip
-            src = fetchzip {
-              sha256 =
-                if sha256 ? ${system} then sha256.${system}
-                else if sha256 ? ${target} then sha256.${target}
-                else sha256;
-              url = with value;
-                if value ? github then "https://github.com/${github}/releases/latest/download/${name}-${target}.zip"
-                else url;
-            };
-            installPhase = ''
-              install -m755 -D ${bin} $out/bin/${name}
-            '';
-          })
-        )
-        (recursiveUpdate (import ./overlay-integrity.nix) {
+        stdenv.mkDerivation {
+          inherit pname version;
+          # since it only available in .zip
+          src = fetchzip (integrity'.${pname} // {
+            url = with value;
+              if value ? github then "https://github.com/${github}/releases/${version}/download/${pname}-${target}.zip"
+              else url;
+          });
+          installPhase = ''
+            install -m755 -D ${bin} $out/bin/${pname}
+          '';
+        })
+        (filterAttrs (name: value: value ? github || value ? url) (recursiveUpdate integrity {
           dlint.github = "denoland/deno_lint";
           dprint.github = "dprint/dprint";
-        });
+        }))
+      ) // {
+        nodeEnv = final.callPackage ./node-env.nix { };
+        esbuild = final.esbuild.override {
+          version = with lib; let
+            node = final.callPackage ./node-packages.nix { };
+            sources = mapAttrs'
+              (name: source: nameValuePair
+                (getName (last (splitString "/" name)))
+                source)
+              node.sources;
+          in
+          sources.esbuild.version;
+        };
+      };
     };
 }
--- overlay-integrity.nix
+++
@@ -1,9 +0,0 @@
-# use `nix run -- nixpkgs#nix-prefetch fetchzip --url -E $URL_FOR_DOWNLOAD`
-{
-  # dprint.sha256.x86_64-linux = "";
-  # dlint.sha256.x86_64-linux = "";
-  # dprint.sha256.x86_64-darwin = "";
-  # dlint.sha256.x86_64-darwin = "";
-  # dprint.sha256.x86_64-windows = "";
-  # dlint.sha256.x86_64-windows = "";
-}
