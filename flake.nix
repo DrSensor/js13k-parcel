610ae81791dc48a20899bd31e8bcded78f760ebd
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-09-13 13:06:54 +0700

    Replace node2nix with naive approach
    It will automatically select package manager by lockfiles found in ./.
    By default it will use npm if no lockfiles found.
    Its possible having multiple package manager if multiple lockfile found.
    
    Caveats: node_modules are not cached nor treated as nix derivation.

--- flake.nix
+++ flake.nix
@@ -10,40 +10,71 @@
 
       # Helpers to instantiate nixpkgs for supported system types.
       give = with nixpkgs.lib; let forAllSystems = f: genAttrs givenSystems (system: f system); in
-      f: forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
-          node = with pkgs; callPackage ./package-lock.nix { };
-        in
-        f { inherit pkgs node; inherit (pkgs) system lib stdenv; });
-
-      # check if node2nix (in devShell) already executed
-      lockExists = builtins.pathExists ./package-lock.nix;
+      f: forAllSystems (system: f (import nixpkgs { inherit system; overlays = [ self.overlay ]; }));
     in
     {
       # nix develop
-      devShell = give ({ pkgs, lib, node, ... }: with pkgs; mkShell {
-        inputsFrom = lib.optionals lockExists [ node.shell ];
-        packages = with nodePackages; [ node2nix ]
-          ++ [ dlint dprint ];
+      devShell = give ({ pkgs, mkShell, lib, ... }: with lib;
+        let packageManagers = with pkgs.nodePackages;
+        (optional (pathExists ./pnpm-lock.yaml) pnpm) ++
+        (optional (pathExists ./yarn.lock) yarn) ++
+        (optional
+          ((hasInfix "slim" pkgs.nodejs) && (
+            (pathExists ./package-lock.json) ||
+            (all (lock: !(pathExists lock)) [ ./yarn.lock ./pnpm-lock.yaml ])
+          ))
+          npm); in
+        mkShell {
+          packages = with pkgs; [ nodejs dlint dprint ] ++ packageManagers
+            # some native packages related to package.json
+            ++ [ vips ] # npm:sharp which used by npm:@parcel/transformer-image
+            ++ [ pkg-config ]; # to make dynamic linking of libs from nixpkgs working
+          shellHook = with pkgs.nodePackages; ''
+            function oninterrupt {
+              mv package.json{.backup,}
+              rm -fr node_modules
+            }
+            cp package.json{,.backup}
+            trap "oninterrupt" EXIT INT
 
-        shellHook = with node.shell;
-          if lockExists then ''
-            [ -d "node_modules" ] || ln -s ${nodeDependencies}/lib/node_modules node_modules
-          '' else ''
-            echo "generating package-lock.nix node-pacakges.nix node-env.nix"
-            node2nix --composition package-lock.nix \
-                     --strip-optional-dependencies \
-                     --development
-            [ -d ".git" ] && git add package-lock.nix node-packages.nix node-env.nix
-
-            echo -e "\nPlease run$(tput bold) nix develop $(tput sgr0)again."
-            exit
+            chmod +x scripts/*
+          '' + (
+            if 0 == count isDerivation packageManagers
+            then ''
+              [ -d node_modules ] || npm install
+            ''
+            else if pathExists ./package-lock.json
+            then ''
+              [ -d node_modules ] || npm install
+              for pkg in ${toString (remove npm (map nodePackages-getName packageManagers))}
+                do $pkg import
+              done
+            ''
+            else ''
+              if [ ! -d node_modules ]
+                then ${nodePackages-getName (head packageManagers)} install
+                for pm in ${toString (map nodePackages-getName (tail packageManagers))}
+                do
+                  [ $pm == npm  ] && npm install --package-lock-only
+                  [ $pm == yarn ] && yarn generate-lock-entry
+                  [ $pm == pnpm ] && pnpm install --lockfile-only
+              done; fi
+            ''
+          ) + ''
+            mv package.json{.backup,}
+            trap - EXIT INT
+            export NODE_PATH=$(realpath ./node_modules)
+            export PATH=$(realpath ./node_modules/.bin):$(realpath ./scripts):$PATH
           '';
-      });
+        });
 
       # extra pkgs
-      overlay = final: prev: with prev.lib; mapAttrs
+      overlay = final: prev: with prev.lib; {
+        lib = prev.lib // rec {
+          nodePackages-prefix = "node_";
+          nodePackages-getName = drv: removePrefix nodePackages-prefix (getName drv);
+        };
+      } // mapAttrs
         (pname: value@{ version ? "latest", bin ? pname, ... }: with final; let
           target = with systems.parse; tripleFromSystem (mkSystemFromString system);
           integrity' = mapAttrs
