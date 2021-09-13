f1575d9cd8334e069e7c8d9d39205acefa575bc7
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-09-13 12:56:58 +0700

    Fix typo

--- integrity.nix
+++ integrity.nix
@@ -9,4 +9,6 @@
 
   # use `nix run -- nixpkgs#nix-prefetch fetchzip --url -E "https://registry.npmjs.org/${packageName}/-/${packageName}-${version}.tgz"`
   # esbuild.sha512 = "";
+  # https://github.com/msteen/nix-prefetch/issues/3#issuecomment-876771025
+  # nix-prefetch '{ sha256 }: (callPackage (import ./default.nix) { }).go-modules.overrideAttrs (_: { modSha256 = sha256; })'
 }
